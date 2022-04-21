71b9ed5a4b666f9cb3fc273757b2a0f0a38b64ad
Michael Camerlynck <71758112+MichaelCamerlynck@users.noreply.github.com>
2022-04-21 09:00:51 +0200

    Update IntroPage.cs

--- Views/IntroPage.cs
+++ Views/IntroPage.cs
@@ -11,7 +11,7 @@ namespace WebScapper.Views
             Console.Clear();
             Art.Print();
             Console.WriteLine("=======================");
-            Console.WriteLine("1. YouTube Scrapping");
+            Console.WriteLine("1. YouTube Scraping");
             Console.WriteLine("2. Job Scrapping");
             Console.WriteLine("3. Flight Price Scrapping");
             Console.WriteLine("Any other key to exit");
